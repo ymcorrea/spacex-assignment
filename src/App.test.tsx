14a231a7e57de809f4df4e1e33c9a815ed92d2dd
botbenz82 <botbenz111@gmail.com>
2023-01-24 22:28:06 +0600

    fresh commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./redux/store/store";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
-    </Provider>
+    </Provider>,
   );
 
   expect(getByText(/learn/i)).toBeInTheDocument();
